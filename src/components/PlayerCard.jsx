5ec906b9a518ea4d5c27dcb89290fe4c0e657666
Sam Kim <samuel.kim.2898@gmail.com>
2025-03-20 11:09:13 -0400

    handle player name edge cases to fix links

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,10 +1,28 @@
 import '../styles/App.css'
 
 function getPlayerProfileUrl(playerId, playerName) {
-    let temp = playerName.toLowerCase();
-    temp = temp.replaceAll(" ", "-");
-    let playerProfileUrl = `https://www.mlb.com/player/${temp}-${playerId}`;
+    let formattedName = getFormattedName(playerName)
+    let playerProfileUrl = `https://www.mlb.com/player/${formattedName}-${playerId}`;
     return playerProfileUrl;
+}
+
+function getFormattedName(playerName) {
+    let res = playerName.toLowerCase();
+    res = res.replaceAll(" ", "-");
+    
+    if (res[playerName.length - 1] === ".") {
+      res = res.substring(0, playerName.length - 1)
+    }
+    res = res.replaceAll(".", "-");
+    res = res.replaceAll("'", "-");
+    res = res.replaceAll("á", "a");
+    res = res.replaceAll("é", "e");
+    res = res.replaceAll("í", "i");
+    res = res.replaceAll("ó", "o");
+    res = res.replaceAll("ú", "u");
+    res = res.replaceAll("ñ", "n");
+    res = res.replaceAll("--", "-");
+    return res
 }
 
 function isPitcher(playerPosition) {
